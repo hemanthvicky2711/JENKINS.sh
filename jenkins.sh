f0bcfe22fb82386ffb598741426df237f53b5c6a
hemanthvicky2711 <hemanthkorukonda99@gmail.com>
2026-03-17 12:52:59 +0530

    setup jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,19 +6,14 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Install Java
-sudo apt install -y openjdk-17-jdk
+sudo yum install java-17-amazon-corretto -y
 java -version
 
-# Install required packages
-sudo apt install -y curl gnupg
-
 # Add Jenkins key
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-| sudo gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.gpg
+sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 
 # Add Jenkins repository
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/" \
-| sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 # Update package list
 sudo apt update -y
@@ -28,7 +23,8 @@ sudo apt install -y jenkins
 
 # Start Jenkins
 sudo systemctl start jenkins
-sudo systemctl enable jenkins
+systemctl status jenkins
+systemctl enable jenkins
 
 # Check status
 sudo systemctl status jenkins
