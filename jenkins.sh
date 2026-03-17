933d527c2a4d792b5cfa07dc98e0f3ba64f5aa61
hemanthvicky2711 <hemanthkorukonda99@gmail.com>
2026-03-17 19:00:09 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Update system
-sudo apt update -y
-sudo apt upgrade -y
+sudo yum update -y
+sudo yum upgrade -y
 
 # Install Java
 sudo yum install java-17-amazon-corretto -y
@@ -16,10 +16,10 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 # Update package list
-sudo apt update -y
+sudo yum update -y
 
 # Install Jenkins
-sudo apt install -y jenkins
+sudo yum install -y jenkins
 
 # Start Jenkins
 sudo systemctl start jenkins
